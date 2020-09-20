f30e6fa2b4f1f69e70385e256911cadaeb9cb66b
Yuanrui Zhang <yzhang@polarisqb.com>
2020-09-20 10:44:11 -0400

    ns->ms

--- HW2/bandwidth_measurement/bandwidth_gauger.c
+++ HW2/bandwidth_measurement/bandwidth_gauger.c
@@ -63,7 +63,7 @@ double calculate_read_write_2_1_time(void) {
 
 void print_msg(const char* title, double elapsed_ns, int times) {
     printf("********************%s*********************\n", title);
-    printf("Time = %lf ns\n", elapsed_ns);
+    printf("Time = %lf ms\n", elapsed_ns / 1000000.0);
     printf("Bandwidth is = %lf GB/s\n\n", ((double)num_elements * num_traversals * times * 8) / (elapsed_ns));
 }
 
