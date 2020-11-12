5249cce23da30dbd9fb48da94c7ce9d9e608586d
Yuanrui Zhang <yuanrui.zhang@duke.edu>
2020-11-11 22:02:55 -0500

    get rid of vectors, using 1d array instead

--- HW5/rainfall/landscape.cpp
+++ HW5/rainfall/landscape.cpp
@@ -5,12 +5,20 @@
 
 #include "landscape.hpp"
 
-Landscape::Landscape(int _dim, const char* filepath) {
-    dim = _dim;
-    points.resize(dim);
-    for(int i = 0; i < dim; ++i) {
-        points[i].resize(dim, 0);
-    }
+Landscape::Landscape(int _dim, const char* filepath): dim(_dim),
+                                                      raindrops(new double[dim * dim]),
+                                                      absorbed_drops(new double[dim * dim]),
+                                                      trickling_directions(new std::unordered_set<Direction>[dim * dim]) {
+    // elevations = std::vector<std::vector<double> > (dim);
+    // raindrops = std::vector<std::vector<double> > (dim);
+    // absorbed_drops = std::vector<std::vector<double> > (dim);
+    // trickling_direction = std::vector<std::vector<std::unordered_set<Direction> > > (dim);
+    // for(int i = 0; i < dim; ++i) {
+    //     elevations[i] = std::vector<double> (dim, 0);
+    //     raindrops[i] = std::vector<double> (dim, 0);
+    //     absorbed_drops[i] = std::vector<double> (dim, 0);
+    // }
+    double* elevations = new double[dim * dim];
     std::ifstream input_file(filepath, std::ifstream::in);
     std::string line;
     int row = 0;
@@ -18,18 +26,42 @@ Landscape::Landscape(int _dim, const char* filepath) {
         std::stringstream ss(line);
         int col;
         for(col = 0; col < dim; ++col) {
-            ss >> points[row][col];
+            ss >> elevations[row * dim + col];
         }
         ++row;
     }
+    print_elevations(elevations);
+    delete[] elevations;
+}
+
+void Landscape::calculate_trickling_drops(int row, int col) {
+    // 正向思考 搞个数组存放东西南北？
+    if(row - 1 >= 0) {
+
+    }
+}
+
+void Landscape::absorb(int row, int col, double absorption_rate) {
+    absorbed_drops[row * dim + col] += std::max(0.0, absorption_rate);
+    raindrops[row * dim + col] = std::max(0.0, raindrops[row * dim + col] - absorption_rate);
 }
 
 // Debug purpose
-void Landscape::print(void) {
+void Landscape::print_elevations(double* elevations) {
     for(int i = 0; i < dim; ++i) {
         for(int j = 0; j < dim; ++j) {
-            std::cout << points[i][j] << ' ';
+            std::cout << elevations[i * dim + j] << ' ';
         }
         std::cout << std::endl;
     }
-} 
+}
+
+void Landscape::insert(void) {
+    trickling_directions[0].insert(Direction::East);
+    std::cout << "insert!\n";
+    for(auto& it : trickling_directions[0]) {
+        if(it == Direction::East){
+            std::cout << "correct" << std::endl;
+        }
+    }
+}
--- HW5/rainfall/landscape.hpp
+++ HW5/rainfall/landscape.hpp
@@ -1,11 +1,43 @@
-#include <vector>
+#include <algorithm>
+#include <unordered_set>
 
+#include <iostream>
+
+enum class Direction {North, East, South, West};
 class Landscape {
     private:
         int dim;
-        std::vector<std::vector<double> > points;
+        // std::vector<std::vector<double> > elevations;
+        // std::vector<std::vector<double> > raindrops;
+        // std::vector<std::vector<double> > absorbed_drops;
+        // std::vector<std::vector<std::unordered_set<Direction> > > trickling_direction;
+        
+        double* raindrops;
+        double* absorbed_drops;
+        std::unordered_set<Direction>* trickling_directions;
+        
+        void receive_rain_drop(int row, int col) {
+            raindrops[row * dim + col]++;
+        }
+
+        void absorb(int row, int col, double absorption_rate);
+
+        void calculate_trickling_drops(int row, int col);
+
+        void trickle(int row, int col);
+
+        // Debug purpose
+        void print_elevations(double* elevations);
 
     public:
         Landscape(int _dim, const char* filepath);
-        void print(void); // Debug purpose
+
+        ~Landscape() {
+            delete[] raindrops;
+            delete[] absorbed_drops;
+            delete[] trickling_directions;
+        }
+
+        // Debug purpose
+        void insert(void);
 };
